1c6f805d7506fe6623923352036403b03cfbd46c
Cristian Falcas <cristi.falcas@gmail.com>
2011-06-02 16:27:13 +0300

    fsag

--- convertors/LibreOfficeMacro.sh
+++ convertors/LibreOfficeMacro.sh
@@ -1,3 +1,3 @@
 #copy all .libreoffice directory to ~/.libreoffice
 
-libreoffice -headless -invisible -nocrashreport -nodefault -nologo -nofirststartwizard -norestore "macro:///Standard.Module1.SaveAsDoc($PATH_TO_FILE)"
+libreoffice -unnaccept=all -headless -invisible -nocrashreport -nodefault -nologo -nofirststartwizard -norestore "macro:///Standard.Module1.SaveAsDoc($PATH_TO_FILE)"
--- our_perl_lib/lib/Mind_work/WikiCommons.pm
+++ our_perl_lib/lib/Mind_work/WikiCommons.pm
@@ -411,8 +411,8 @@ sub generate_html_file {
     eval {
 	local $SIG{ALRM} = sub { die "alarm\n" };
 	alarm 46800; # 13 hours
-	system("python $real_path/convertors/unoconv -l -p 8100 2>&1 /dev/null &") == 0 or die "unoconv failed: $?";
-	sleep 2;
+# 	system("python $real_path/convertors/unoconv -l -p 8100 2>&1 /dev/null &") == 0 or die "unoconv failed: $?";
+# 	sleep 2;
 	system("python", "$real_path/convertors/unoconv", "-f", "html", "$doc_file") == 0 or die "unoconv failed: $?";
 	alarm 0;
     };
@@ -422,9 +422,10 @@ sub generate_html_file {
 	eval {
 	    local $SIG{ALRM} = sub { die "alarm\n" };
 	    alarm 46800; # 13 hours
-	    system("python $real_path/convertors/unoconv -l -p 8100 2>&1 /dev/null &") == 0 or die "unoconv failed: $?";
-	    sleep 2;
-	    system("/opt/jre1.6.0/bin/java", "-jar", "$real_path/convertors/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar", "-f", "html", "$doc_file") == 0 or die "jodconverter failed: $?";
+# 	    system("python $real_path/convertors/unoconv -l -p 8100 2>&1 /dev/null &") == 0 or die "unoconv failed: $?";
+# 	    sleep 2;
+# 	    system("/opt/jre1.6.0/bin/java", "-jar", "$real_path/convertors/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar", "-f", "html", "$doc_file") == 0 or die "jodconverter failed: $?";
+	    system("libreoffice", "-unnaccept=all", "-headless", "-invisible", "-nocrashreport", "-nodefault", "-nologo", "-nofirststartwizard", "-norestore", "-convert-to", "html:HTML (StarWriter)", "$doc_file") == 0 or die "unoconv failed: $?";
 	    alarm 0;
 	};
 	$status = $@;
